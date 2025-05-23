93e5a421c77686428b30ac79495a4beb6d05e169
Андрей Полищук <dcnfandrey@mail.ru>
2025-05-23 21:56:41 +0300

    Templation DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,45 +6,25 @@ using std::endl;
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+template <typename T>
+void Print(T arr[], const int n);
+template<typename T>
+void Sort(T arr[], const int n);
+template<typename T>
+T Sum(T arr[], const int n);			//Возвращает сумму элементов массива;
 
-void Print(int arr[], const int n);
+template<typename T>
+double Avg(T arr[], const int n);			//Возвращает среднее-арифметическое элементов массива;
 
-void Sort(int arr[], const int n);
+template<typename T>
+T MinValueIn(T arr[], const int n);	//Возвращает минимальное значение в массиве;
 
-int Sum(int arr[], const int n);			//Возвращает сумму элементов массива;
-float Sum(float arr[], const int n);			//Возвращает сумму элементов массива;
-double Sum(double arr[], const int n);			//Возвращает сумму элементов массива;
-char Sum(char arr[], const int n);			//Возвращает сумму элементов массива;
-
-
-double Avg(int arr[], const int n);			//Возвращает среднее-арифметическое элементов массива;
-double Avg(float arr[], const int n);			//Возвращает среднее-арифметическое элементов массива;
-double Avg(double arr[], const int n);			//Возвращает среднее-арифметическое элементов массива;
-double Avg(char arr[], const int n);			//Возвращает среднее-арифметическое элементов массива;
-
-
-int MinValueIn(int arr[], const int n);	//Возвращает минимальное значение в массиве;
-float MinValueIn(float arr[], const int n);	//Возвращает минимальное значение в массиве;
-double MinValueIn(double arr[], const int n);	//Возвращает минимальное значение в массиве;
-char MinValueIn(char arr[], const int n);	//Возвращает минимальное значение в массиве;
-
-
-int MaxValueIn(int arr[], const int n);	//Возвращает максимальное значение в массиве;
-float MaxValueIn(float arr[], const int n);	//Возвращает максимальное значение в массиве;
-double MaxValueIn(double arr[], const int n);	//Возвращает максимальное значение в массиве;
-char MaxValueIn(char arr[], const int n);	//Возвращает максимальное значение в массиве;
-
-
-void ShiftLeft(int arr[], const int n, int shift = 0);		//Циклически сдвигает массив на заданное количество элементов влево;
-void ShiftLeft(float arr[], const int n, int shift = 0);		//Циклически сдвигает массив на заданное количество элементов влево;
-void ShiftLeft(double arr[], const int n, int shift = 0);		//Циклически сдвигает массив на заданное количество элементов влево;
-void ShiftLeft(char arr[], const int n, int shift = 0);		//Циклически сдвигает массив на заданное количество элементов влево;
-
-
-void ShiftRight(int arr[], const int n, int shift = 0);
-void ShiftRight(float arr[], const int n, int shift = 0);
-void ShiftRight(double arr[], const int n, int shift = 0);
-void ShiftRight(char arr[], const int n, int shift = 0);
+template<typename T>
+T MaxValueIn(T arr[], const int n);	//Возвращает максимальное значение в массиве;
+template<typename T>
+void ShiftLeft(T arr[], const int n, int shift = 0);		//Циклически сдвигает массив на заданное количество элементов влево;
+template<typename T>
+void ShiftRight(T arr[], const int n, int shift = 0);
 
 // 60 строк кода
 void main()
@@ -106,8 +86,8 @@ void FillRand(double arr[], const int n, int minRand, int maxRand)
 	}
 }
 
-
-void Print(int arr[], const int n)
+template <typename T>
+void Print(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -116,8 +96,8 @@ void Print(int arr[], const int n)
 	cout << endl;
 }
 
-
-void Sort(int arr[], const int n)
+template <typename T>
+void Sort(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -125,7 +105,7 @@ void Sort(int arr[], const int n)
 		{
 			if (arr[j] < arr[i])
 			{
-				int buffer = arr[i];
+				T buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
@@ -133,97 +113,31 @@ void Sort(int arr[], const int n)
 	}
 }
 
-
-int Sum(int arr[], const int n)			//Возвращает сумму элементов массива;
+template<typename T>
+T Sum(T arr[], const int n)			//Возвращает сумму элементов массива;
 {
-	int s = 0;
+	T s = 0;
 	for (int i = 0; i < n; i++) s += arr[i];
 	return s;
 }
 
-float Sum(float arr[], const int n)			//Возвращает сумму элементов массива;
-{
-	float s = 0;
-	for (int i = 0; i < n; i++) s += arr[i];
-	return s;
-}
-
-double Sum(double arr[], const int n)			//Возвращает сумму элементов массива;
-{
-	double s = 0;
-	for (int i = 0; i < n; i++) s += arr[i];
-	return s;
-}
-
-char Sum(char arr[], const int n)			//Возвращает сумму элементов массива;
-{
-	char s = 0;
-	for (int i = 0; i < n; i++) s += arr[i];
-	return s;
-}
-
-double Avg(int arr[], const int n)			//Возвращает среднее-арифметическое элементов массива;
+template<typename T>
+double Avg(T arr[], const int n)			//Возвращает среднее-арифметическое элементов массива;
 {
 	return (double)Sum(arr, n) / n;
 }
-
-double Avg(float arr[], const int n)			//Возвращает среднее-арифметическое элементов массива;
+template<typename T>
+T MinValueIn(T arr[], const int n)	//Возвращает минимальное значение в массиве;
 {
-	return (double)Sum(arr, n) / n;
-}
-double Avg(double arr[], const int n)		//Возвращает среднее-арифметическое элементов массива;
-{
-	return (double)Sum(arr, n) / n;
-}
-
-double Avg(char arr[], const int n)			//Возвращает среднее-арифметическое элементов массива;
-{
-	return (double)Sum(arr, n) / n;
-}
-
-
-int MinValueIn(int arr[], const int n)	//Возвращает минимальное значение в массиве;
-{
-	int min_value = arr[0];
+	T min_value = arr[0];
 	for (int i = 1; i < n; i++)
 	{
 		if (arr[i] < min_value) min_value = arr[i];
 	}
 	return min_value;
 }
-
-float MinValueIn(float arr[], const int n)	//Возвращает минимальное значение в массиве;
-{
-	float min_value = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] < min_value) min_value = arr[i];
-	}
-	return min_value;
-}
-
-double MinValueIn(double arr[], const int n)	//Возвращает минимальное значение в массиве;
-{
-	double min_value = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] < min_value) min_value = arr[i];
-	}
-	return min_value;
-}
-
-char MinValueIn(char arr[], const int n)	//Возвращает минимальное значение в массиве;
-{
-	char min_value = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] < min_value) min_value = arr[i];
-	}
-	return min_value;
-}
-
-
-int MaxValueIn(int arr[], const int n)	//Возвращает максимальное значение в массиве;
+template<typename T>
+T MaxValueIn(T arr[], const int n)	//Возвращает максимальное значение в массиве;
 {
 		int max_value = arr[0];
 		for (int i = 1; i < n; i++)
@@ -233,40 +147,10 @@ int MaxValueIn(int arr[], const int n)	//Возвращает максималь
 		return max_value;
 }
 
-float MaxValueIn(float arr[], const int n)	//Возвращает максимальное значение в массиве;
+template<typename T>
+void ShiftLeft(T arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов влево;
 {
-		float max_value = arr[0];
-		for (int i = 1; i < n; i++)
-		{
-			if (arr[i] > max_value) max_value = arr[i];
-		}
-		return max_value;
-}
-
-double MaxValueIn(double arr[], const int n)	//Возвращает максимальное значение в массиве;
-{
-	double max_value = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] > max_value) max_value = arr[i];
-	}
-	return max_value;
-}
-
-char MaxValueIn(char arr[], const int n)	//Возвращает максимальное значение в массиве;
-{
-	char max_value = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] > max_value) max_value = arr[i];
-	}
-	return max_value;
-}
-
-
-void ShiftLeft(int arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов влево;
-{
-	int t;
+	T t;
 	for (int i = 0; i < shift % n; i++)
 	{
 		t = arr[0];
@@ -277,87 +161,8 @@ void ShiftLeft(int arr[], const int n, int shift)		//Циклически сдв
 		arr[n - 1] = t;
 	}
 }
-void ShiftLeft(float arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов влево;
-{
-	float t;
-	for (int i = 0; i < shift % n; i++)
-	{
-		t = arr[0];
-		for (int j = 1; j < n; j++)
-		{
-			arr[j - 1] = arr[j];
-		}
-		arr[n - 1] = t;
-	}
-}
-void ShiftLeft(double arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов влево;
-{
-	double t;
-	for (int i = 0; i < shift % n; i++)
-	{
-		t = arr[0];
-		for (int j = 1; j < n; j++)
-		{
-			arr[j - 1] = arr[j];
-		}
-		arr[n - 1] = t;
-	}
-}
-void ShiftLeft(char arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов влево;
-{
-	char t;
-	for (int i = 0; i < shift % n; i++)
-	{
-		t = arr[0];
-		for (int j = 1; j < n; j++)
-		{
-			arr[j - 1] = arr[j];
-		}
-		arr[n - 1] = t;
-	}
-}
-
-
-void ShiftRight(int arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов вправо;
+template<typename T>
+void ShiftRight(T arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов вправо;
 {
 	ShiftLeft(arr, n, n - shift);
-}
-void ShiftRight(float arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов вправо;
-{
-	float buffer;
-	for (int i = 0; i < shift % n; i++)
-	{
-		buffer = arr[n - 1];
-		for (int j = n - 1; j > 0; j--)
-		{
-			arr[j] = arr[j - 1];
-		}
-		arr[0] = buffer;
-	}
-}
-void ShiftRight(double arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов вправо;
-{
-	double buffer;
-	for (int i = 0; i < shift % n; i++)
-	{
-		buffer = arr[n - 1];
-		for (int j = n - 1; j > 0; j--)
-		{
-			arr[j] = arr[j - 1];
-		}
-		arr[0] = buffer;
-	}
-}
-void ShiftRight(char arr[], const int n, int shift)		//Циклически сдвигает массив на заданное количество элементов вправо;
-{
-	char buffer;
-	for (int i = 0; i < shift % n; i++)
-	{
-		buffer = arr[n - 1];
-		for (int j = n - 1; j > 0; j--)
-		{
-			arr[j] = arr[j - 1];
-		}
-		arr[0] = buffer;
-	}
 }
